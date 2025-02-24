9070a7e36963e30efd423c5f6cf5cef97597eb74
Matthew <matthew@loopspeed.co.uk>
2025-02-24 20:59:48 +0000

    Update EnergyTransfer.tsx

--- components/examples/three/energyTransfer/EnergyTransfer.tsx
+++ components/examples/three/energyTransfer/EnergyTransfer.tsx
@@ -1,34 +1,40 @@
-'use client'
-import { useGSAP } from '@gsap/react'
-import { PerformanceMonitor, PerformanceMonitorApi, Stats, usePerformanceMonitor } from '@react-three/drei'
-import { Canvas, useThree } from '@react-three/fiber'
-import { Bloom, EffectComposer } from '@react-three/postprocessing'
-import gsap from 'gsap'
-import { KernelSize } from 'postprocessing'
-import React, { type FC, useMemo, useState } from 'react'
-import { Color, type Vector3Tuple } from 'three'
+"use client";
+import { useGSAP } from "@gsap/react";
+import {
+  PerformanceMonitor,
+  PerformanceMonitorApi,
+  Stats,
+  usePerformanceMonitor,
+} from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import gsap from "gsap";
+import { KernelSize } from "postprocessing";
+import React, { type FC, useMemo, useState } from "react";
+import { Color, type Vector3Tuple } from "three";
 
-import Camera from '@/components/examples/three/Camera'
+import Camera from "@/components/examples/three/Camera";
 
-import EnergyTunnel, { HALF_TUNNEL_LENGTH } from './cylinder/EnergyTunnel'
-import Environment from './environment/Environment'
-import EnergyTunnelPoints from './points/EnergyPoints'
-import EnergySphere, { SPHERE_RADIUS } from './sphere/EnergySphere'
+import EnergyTunnel, { HALF_TUNNEL_LENGTH } from "./cylinder/EnergyTunnel";
+import Environment from "./environment/Environment";
+import EnergyTunnelPoints from "./points/EnergyPoints";
+import EnergySphere, { SPHERE_RADIUS } from "./sphere/EnergySphere";
 
-gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(useGSAP);
 
-type Props = {}
+const PINK_COLOUR = new Color("#37F3FF");
+const CYAN_COLOUR = new Color("#FF6DF5");
 
-const PINK_COLOUR = new Color('#37F3FF')
-const CYAN_COLOUR = new Color('#FF6DF5')
+const EnergyTransfer: FC = () => {
+  const size = useThree((s) => s.size);
+  const isLandscape = size.width > size.height;
+  const rotation: Vector3Tuple = useMemo(
+    () => (isLandscape ? [0, 0, Math.PI / 2] : [0, 0, 0]),
+    [isLandscape]
+  );
 
-const EnergyTransfer: FC<Props> = ({}) => {
-  const size = useThree((s) => s.size)
-  const isLandscape = size.width > size.height
-  const rotation: Vector3Tuple = useMemo(() => (isLandscape ? [0, 0, Math.PI / 2] : [0, 0, 0]), [isLandscape])
-
-  const seedA = useMemo(() => Math.random(), [])
-  const seedB = useMemo(() => Math.random(), [])
+  const seedA = useMemo(() => Math.random(), []);
+  const seedB = useMemo(() => Math.random(), []);
 
   return (
     <group rotation={rotation} position={[0, 0, isLandscape ? 0 : -1]}>
@@ -47,12 +53,12 @@ const EnergyTransfer: FC<Props> = ({}) => {
         position={[0, HALF_TUNNEL_LENGTH + SPHERE_RADIUS, 0]}
       />
     </group>
-  )
-}
+  );
+};
 
 type CanvasProps = {
-  className?: string
-}
+  className?: string;
+};
 
 const EnergyTransferCanvas: FC<CanvasProps> = ({ className }) => {
   return (
@@ -64,34 +70,35 @@ const EnergyTransferCanvas: FC<CanvasProps> = ({ className }) => {
         gl={{
           alpha: false,
           antialias: false,
-          powerPreference: 'high-performance',
+          powerPreference: "high-performance",
           stencil: false,
           depth: false,
-        }}>
+        }}
+      >
         <PerformanceMonitor>
           <Environment />
           <EnergyTransfer />
           <Camera />
           <Postprocessing />
         </PerformanceMonitor>
-        {process.env.NODE_ENV === 'development' && <Stats />}
+        {process.env.NODE_ENV === "development" && <Stats />}
       </Canvas>
     </main>
-  )
-}
+  );
+};
 
 const Postprocessing: FC = () => {
-  const [isEnabled, setIsEnabled] = useState(true)
+  const [isEnabled, setIsEnabled] = useState(true);
 
   const onIncline = (api: PerformanceMonitorApi) => {
-    setIsEnabled(api.fps > 50)
-  }
+    setIsEnabled(api.fps > 50);
+  };
 
   const onDecline = (api: PerformanceMonitorApi) => {
-    setIsEnabled(!(api.fps > 50))
-  }
+    setIsEnabled(!(api.fps > 50));
+  };
 
-  usePerformanceMonitor({ onIncline, onDecline })
+  usePerformanceMonitor({ onIncline, onDecline });
 
   return (
     <EffectComposer enabled={isEnabled}>
@@ -104,7 +111,7 @@ const Postprocessing: FC = () => {
         kernelSize={KernelSize.MEDIUM}
       />
     </EffectComposer>
-  )
-}
+  );
+};
 
-export default EnergyTransferCanvas
+export default EnergyTransferCanvas;
